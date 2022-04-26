0d7aa25fb69e6dfa1437a4bef333f990398c7a22
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-26 16:09:40 -0300

    register

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,35 @@
 const router = require("express").Router()
-///model
+//modules
+const bcrypt = require("bcrypt")
+
+//model
 const User = require("../models/User")
 
 //REGISTER
 router.post("/register", async (req, res) => {
     
+
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashPassword
+        })
+
+        //save user and respond
+        const user = await newUser.save()
+        
+        res.status(200).json(user)
+    
+    } catch (error) {
+        res.status(500).json(error)        
+    }
+
 })
 
 module.exports = router
