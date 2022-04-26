eb5845c0357c0daa464febf22ac5dac24fdb5799
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-26 20:12:49 -0300

    followers & followings user

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -54,9 +54,54 @@ router.delete("/:id", async (req, res) => {
 
 //get a user
 router.get("/:id", async (req, res) => {
-    
+    try {
+        
+        const user = await User.findById(req.params.id)
+        const {password, updatedAt, ...other} = user._doc //_doc is the all documente of data
+        //other is all data except password and updatedAt
+
+        res.status(200).json(other)
+
+    } catch (error) {
+        
+        res.status(500).json(error)
+
+    }
 })
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+
+    if(req.body.userId !== req.params.id){
+
+        try {
+            
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            
+            if(!user.followers.includes(req.body.userId)){ //PT - varificar ser o id do usuário já está incluiso no followers
+
+                await user.updateOne({$push: {followers: req.body.userId}}) //PT - Então adicionar o id do usuário no dado followers
+                //PT - o metodo push server da mesma forma que no array js convencional
+                
+                await currentUser.updateOne({$push: {followings: req.params.id}}) //PT - E adicionar nos seguindos do usuário que clicou
+                res.status(200).json("user has been followed")
+
+            } else {
+                res.status(403).json("you allready follow this user")
+            }
+
+        } catch (error) {
+            
+            res.status(500).json(error)
+
+        }
+
+    } else {
+        res.status(403).json("you cant follow yourserlf")
+    }
+
+})
+
 
 //unfollow a user
 
